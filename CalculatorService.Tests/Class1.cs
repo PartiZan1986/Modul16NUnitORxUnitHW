c0f70aeaf493c3ee2f4a28e30f676e2bc3df8325
Yaroslav <kay3@yandex.ru>
2024-02-01 12:40:36 +0300

    Add project files.

---
+++ CalculatorService.Tests/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Additional_MustReturnValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(10, 20) == 30);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(20, 10) == 10);
+        }
+
+        [Test]
+        public void Multiplication_MustReturnValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(20, 10) == 200);
+        }
+
+        [Test]
+        public void Division_MustReturnValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(20, 0));
+        }
+    }   
+}
---
+++ Modul16NUnitORxUnitHW/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Modul16NUnitORxUnitHW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Modul16NUnitORxUnitHW")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0a7e2e0-59a0-415d-9fc8-2c9dbf5a70dc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
